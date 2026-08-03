7ea8271d53d4000deed9a4a8b17303fcf7c98692
Abrar Hossain <tonmoyallalong@gmail.com>
2026-08-03 12:36:50 +0600

    created necessary module folder structurs for genre,media,review,watchlist, completedmedia

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.route";
+import mediaRoutes from "./modules/media/media.route";
 
 const app: express.Application = express();
 
 app.use(express.json());
 
-// Auth Routes যুক্ত করা হলো
+// Auth Routes
 app.use("/api/v1/auth", authRoutes);
+
+// Media Routes
+app.use("/api/v1/media", mediaRoutes);
 
 export default app;
